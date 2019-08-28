0ec08645e1b423d0395b688ac38d5f8579c7c4a6
siykt <cnsiykt@163.com>
2019-08-28 14:02:05 +0800

    feat(init): 移动fb登录至新模块

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import Vue from 'vue';
 import App from './view/everyday_order/App.vue';
 
-// (function (d, s, id) {
-//     var js,
-//         fjs = d.getElementsByTagName(s)[0];
-//     if (d.getElementById(id)) return;
-//     js = d.createElement(s);
-//     js.id = id;
-//     js.src = "https://connect.facebook.net/en_US/sdk.js";
-//     fjs.parentNode.insertBefore(js, fjs);
-// })(document, "script", "facebook-jssdk");
-// window.fbAsyncInit = function () {
-//     FB.init({
-//         appId: "610033166141581",
-//         cookie: true, // 启用cookie
-//         xfbml: true, // 解析此页面上的社交插件
-//         version: "v2.8" // 使用图形api 2.8版本
-//     });
-// };
-
 new Vue({
     el: '#app',
     template: '<App/>',
---
+++ src/view/FB/fb.init.js
@@ -0,0 +1,16 @@
+export default (function (d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+})(document, "script", "facebook-jssdk");
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "610033166141581",
+    cookie: true, // 启用cookie
+    xfbml: true, // 解析此页面上的社交插件
+    version: "v2.8" // 使用图形api 2.8版本
+  });
+};
