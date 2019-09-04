6e182f47fa6ce239c299e63bc74dbdb0fee8e7ff
siykt <cnsiykt@163.com>
2019-09-04 18:07:20 +0800

    feat: 新增贵族中心页面

---
+++ src/view/buyvipindex/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+<script>
+import getApi from "../../modules/getApi";
+
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    getApi(type, data, resolve, reject) {
+      $.ajax({
+        url: `./index.php?g=appapi&m=Mall&a=${type}`,
+        data,
+        type: "GET",
+        dataType: 'json',
+        success(res) {
+          res && res.info && resolve
+            ? resolve(res.info)
+            : reject(res);
+        },
+        error(e) {
+          console.log(e);
+        }
+      });
+      return this;
+    },
+    buyvip(type) {
+      this.getApi(
+        "buyvip",
+        {
+          uid: "25557",
+          token: "1"
+        },
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  },
+  created() {
+    this.getApi(
+      "vipindex",
+      {
+        uid: "25557"
+      },
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+};
+</script>
+<style src="./buyvipindex.scss" lang="scss"></style>
