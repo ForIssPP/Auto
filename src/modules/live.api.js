af12e87c0b46b0c11faeba9d0ce05672aa7e7be4
siykt <cnsiykt@163.com>
2019-09-05 15:44:44 +0800

    feat: 修改API结构

--- src/modules/live.api.js
+++ src/modules/live.api.js
@@ -1,23 +1,37 @@
-export default {
-  goBag(msg) {
-    try {
-      window.mobile ? mobile.goBag(msg || null) : window.webkit.messageHandlers.webRefreshEnable.postMessage(msg || null);
-    } catch (e) {
-      console.log(e)
-    }
-  },
-  closeWebview(msg) {
-    try {
-      window.mobile ? mobile.close() : window.webkit.messageHandlers.closeWebview.postMessage(msg || null);
-    } catch (e) {
-      console.log(e)
-    }
-  },
-  goRecharge(msg) {
-    try {
-      window.mobile ? mobile.recharge() : window.webkit.messageHandlers.goRecharge.postMessage(msg || null);
-    } catch (e) {
-      console.log(e)
-    }
+/**
+ * 打开背包方法
+ * 
+ * @param {any} msg 交互信息
+ */
+export function goBag(msg) {
+  try {
+    window.mobile ? mobile.goBag(msg || null) : window.webkit.messageHandlers.webRefreshEnable.postMessage(msg || null);
+  } catch (e) {
+    console.log(e)
   }
 }
+/**
+ * 关闭视图方法
+ * 
+ * @param {any} msg 交互信息
+ */
+export function closeWebview(msg) {
+  try {
+    window.mobile ? mobile.close() : window.webkit.messageHandlers.closeWebview.postMessage(msg || null);
+  } catch (e) {
+    console.log(e)
+  }
+}
+/**
+ * 切换至充值页面
+ * 
+ * @param {any} msg 交互信息
+ */
+export function goRecharge(msg) {
+  try {
+    window.mobile ? mobile.recharge() : window.webkit.messageHandlers.goRecharge.postMessage(msg || null);
+  } catch (e) {
+    console.log(e)
+  }
+}
+
