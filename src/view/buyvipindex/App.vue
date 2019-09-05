6f93588bab651802bfcb00be424476cd11cd6764
siykt <cnsiykt@163.com>
2019-09-05 15:44:59 +0800

    feat: 添加所需组件

--- src/view/buyvipindex/App.vue
+++ src/view/buyvipindex/App.vue
@@ -1,26 +1,44 @@
 <template>
   <div id="app">
-
+    <div class="content">
+      <contentSwiper :contentList="vipRuleList" :imgList="vipimgList" :nameList="vipNameList"></contentSwiper>
+    </div>
+    <div class="buy">
+      <btnSwiper :texts="buyTexts" @clickBtn="function() { console.log(1) }"></btnSwiper>
+    </div>
   </div>
 </template>
 <script>
 import getApi from "../../modules/getApi";
+import { goRecharge } from "../../modules/live.api";
+import contentSwiper from "./contentSwiper.vue";
+import btnSwiper from "./btnSwiper.vue";
+import vipRule from "./vip_rule.json";
+import vipimgList from "./vip_info.json";
 
 export default {
   data() {
-    return {};
+    return {
+      vipRuleList: [],
+      vipNameList: [],
+      vipimgList,
+      buyTexts: ["100/月 赠送8000魔法币", "续费可获得1w魔法币"]
+    };
+  },
+  components: {
+    contentSwiper,
+    btnSwiper
   },
   methods: {
+    goRecharge,
     getApi(type, data, resolve, reject) {
       $.ajax({
         url: `./index.php?g=appapi&m=Mall&a=${type}`,
         data,
         type: "GET",
-        dataType: 'json',
+        dataType: "json",
         success(res) {
-          res && res.info && resolve
-            ? resolve(res.info)
-            : reject(res);
+          res && resolve ? resolve(res) : reject(res);
         },
         error(e) {
           console.log(e);
@@ -28,15 +46,21 @@ export default {
       });
       return this;
     },
-    buyvip(type) {
+    buyvip(vipid) {
       this.getApi(
         "buyvip",
         {
           uid: "25557",
-          token: "1"
+          token: "1",
+          vipid
         },
         res => {
           console.log(res);
+          if (res.code === 1024) {
+            this.goRecharge();
+          } else {
+            layer.msg(res.msg);
+          }
         },
         err => {
           console.log(err);
@@ -51,7 +75,19 @@ export default {
         uid: "25557"
       },
       res => {
-        console.log(res);
+        this.vipNameList = res.info.list.map(e => {
+          return e.name;
+        });
+        this.vipRuleList = res.info.list.map(e => {
+          let obj = JSON.parse(JSON.stringify(vipRule[3]));
+          obj.text = obj.text.replace(
+            /..身份专属勋章/,
+            e.name + "身份专属勋章"
+          );
+          let reList = vipRule.slice(0, e.canuse);
+          reList[3] = obj;
+          return reList;
+        });
       },
       err => {
         console.log(err);
---
+++ src/view/buyvipindex/btnSwiper.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="buy-box">
+    <div class="text-box">
+      <p v-for="text in texts" :key="text.id">{{ text }}</p>
+    </div>
+    <btnClick :className="'buy-btn'" :name="'开通骑士'" @clickBtn="$emit('clickBtn')"></btnClick>
+  </div>
+</template>
+<script>
+import btnClick from "../everyday_order/btnClick.vue";
+
+export default {
+  props: {
+    texts: {
+      type: Array,
+    },
+  },
+  components: {
+    btnClick
+  }
+};
+</script>
