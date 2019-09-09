ecc5204cc03340a7ee9260ee31661adf7089b778
siykt <cnsiykt@163.com>
2019-09-09 10:45:11 +0800

    feat: buyvipcharge init

---
+++ src/view/buyvipcharge/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <div class="title-box">
+      <AvatarTitle></AvatarTitle>
+      <div class="bar">
+        <h2>魔法币支付</h2>
+        <p>
+          当前余额：
+          <span>{{ money }}</span>魔法币
+        </p>
+      </div>
+    </div>
+    <div class="chose-box">
+      <div class="bar">
+        <h2>选择开通/续费项目</h2>
+        <p>使用有效期{{ day }}天</p>
+      </div>
+    </div>
+    <div class="buy">
+      <btnClick :name="'1万魔法币 立即支付'"></btnClick>
+    </div>
+  </div>
+</template>
+<script>
+import getQueryVariable from "../../modules/getQueryVariable";
+import AvatarTitle from "./AvatarTitle.vue";
+import rule from "./rule.json";
+import getApi from "../buyvipindex/getApiFromBuyVipIndex";
+import btnClick from "../page/btnClick.vue";
+
+export default {
+  data() {
+    return {
+      chose: 1,
+      money: 0,
+      day: rule.day
+    };
+  },
+  components: {
+    AvatarTitle,
+    btnClick
+  },
+  methods: {
+    getQueryVariable,
+    getApi,
+  },
+  created() {
+    getQueryVariable("chose")
+      ? (this.chose = getQueryVariable("chose"))
+      : (this.chose = 1);
+  }
+};
+</script>
---
+++ src/view/buyvipcharge/AvatarTitle.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="avatar-title">
+    <div class="bg"></div>
+    <div class="userinfo">
+      <img :src="imgSrc" alt="avatar" class="avatar" />
+      <div class="uid" v-if="uid">{{ uid }}</div>
+      <p v-if="username" class="username">{{ username }}</p>
+      <p v-for="(item, index) in other" :key="index">{{ item }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    imgSrc: {
+      type: String,
+      default: ""
+    },
+    uid: {
+      type: String,
+      default: ""
+    },
+    username: {
+      type: String,
+      default: ""
+    },
+    other: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
