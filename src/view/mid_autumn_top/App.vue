1a6045afc17683ecefaaeb2ebed9894936a84bab
siykt <cnsiykt@163.com>
2019-09-11 17:43:57 +0800

    feat: 中秋活动第二版

--- src/view/mid_autumn_top/App.vue
+++ src/view/mid_autumn_top/App.vue
@@ -27,11 +27,11 @@
     <div class="received">
       <div class="img-box">
         <div class="bg"></div>
-        <img :src="'https://iph.href.lu/160x140'" alt="reward" />
+        <img v-if="receivedImageSrc" :src="receivedImageSrc" alt="reward" />
       </div>
       <p class="received-text">{{ rewardText }}</p>
       <p class="received-money">我的魔法币账户累计： {{ money / 1e4 }}万</p>
-      <btnClick :name="''" :className="'received-btn'" @btnClist="received"></btnClick>
+      <btnClick :name="''" :className="btnActive" @btnClick="received"></btnClick>
     </div>
     <div id="rule">
       <div class="rule-box">
@@ -39,6 +39,9 @@
           <rule :title="rewardTitle" :ruleTexts="rewardTextList"></rule>
         </div>
       </div>
+    </div>
+    <div id="alert">
+      <img :src="alertSrc" />
     </div>
   </div>
 </template>
@@ -55,7 +58,7 @@ import btnClick from "../page/btnClick.vue";
 export default {
   data() {
     return {
-      show: false,
+      show: true,
       ruleTitle: ruleJSON.title,
       ruleTexts: ruleJSON.text,
       rewardTitle: rewardJSON.title,
@@ -63,6 +66,12 @@ export default {
       tops: [],
       titleList: ["排行", "头像", "名字", "ID", "魔法值"],
       rewardText: "暂无可领取奖励",
+      lock: true,
+      btnActive: "received-btn ok",
+      receivedType: 0,
+      receivedImageSrc: "",
+      receivedMsg: "",
+      alertSrc: rewardJSON.receivedImagesSrc[4],
       money: 0
     };
   },
@@ -70,7 +79,43 @@ export default {
     getApi,
     imageGetError,
     received() {
-      console.log("%c received", "color: red");
+      if (this.lock) {
+        return;
+      }
+      if (this.receivedMsg) {
+        return layer.msg(this.receivedMsg);
+      }
+      this.getApi(
+        "Activity.getPrizeMid",
+        {
+          uid: query("uid") || "10000",
+          token: query("token") || "1234567",
+          type: this.receivedType
+        },
+        res => {
+          if (this.receivedType) {
+            this.receivedType = 0;
+            this.rewardText = "坐骑扫帚(7天) x 1";
+          } else if (res.code !== 1005) {
+            this.receivedMsg = res.msg;
+            this.btnActive = "received-btn";
+            this.rewardText = "已领取所有奖励";
+          }
+
+          layer.open({
+            title: "",
+            type: 1,
+            area: ["5.4rem", "3.2rem"],
+            content: $("#alert"),
+            cancel() {
+              layer.msg(res.msg);
+            }
+          });
+        },
+        err => {
+          console.log(err);
+        }
+      );
     }
   },
   components: {
@@ -83,25 +128,59 @@ export default {
       "Activity.midAutumnData",
       null,
       res => {
-        console.log("res", res);
         this.tops = res;
+        this.show = false;
       },
       err => {
         console.log("err", err);
       }
-    ).getApi(
-      "Activity.midPrizeInit",
-      {
-        uid: query("uid") || 10000,
-        token: query("tolen") || 1234567
-      },
-      res => {
-        console.log('res 2', res);
-      },
-      err => {
-        console.log(err);
-      }
-    );
+    )
+      .getApi(
+        "Activity.midAutumnMoney",
+        {
+          uid: query("uid") || "10000",
+          token: query("token") || "1234567"
+        },
+        res => {
+          this.money = parseInt(res[0].money);
+        },
+        err => {
+          console.log(err);
+        }
+      )
+      .getApi(
+        "Activity.midPrizeInit",
+        {
+          uid: query("uid") || "10000",
+          token: query("token") || "1234567"
+        },
+        res => {
+          console.log(res);
+          if (res.prize) {
+            this.lock = false;
+            if (res.prize.coinPrize) {
+              this.rewardText = "VIP(7天) x 1";
+              this.receivedImageSrc =
+                rewardJSON.receivedImagesSrc[
+                  this.money < 1e6 ? 0 : this.money < 2e6 ? 1 : 2
+                ];
+              this.receivedType = 1;
+            } else if (res.prize.signPrize) {
+              this.rewardText = "坐骑扫帚(7天) x 1";
+              this.receivedImageSrc = rewardJSON.receivedImagesSrc[3];
+            } else {
+              this.btnActive = "received-btn";
+              this.rewardText = "";
+            }
+          } else {
+            this.btnActive = "received-btn";
+            this.rewardText = "";
+          }
+        },
+        err => {
+          console.log(err);
+        }
+      );
   }
 };
 </script>
