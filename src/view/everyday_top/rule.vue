4f7d72773bc6b53b6053b97b0fe9a89158cf62ac
siykt <cnsiykt@163.com>
2019-09-16 10:34:46 +0800

    fix: 修改展示

--- src/view/everyday_top/rule.vue
+++ src/view/everyday_top/rule.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="rule">
-    <h1 class="title">{{ title }}</h1>
+    <h1 class="title" v-html="title"></h1>
     <p class="rule-text" v-for="ruleText in ruleTexts" :key="ruleText.id" v-html="ruleText"></p>
   </div>
 </template>
