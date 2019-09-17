43c582c5b32cd71f143604640e5aa8ebf7176218
siykt <cnsiykt@163.com>
2019-09-17 11:19:58 +0800

    feat: test SVGA WEB

--- src/view/SVGA Web/App.vue
+++ src/view/SVGA Web/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <div></div>
+  <div id="app">
+    <div id="demo"></div>
+  </div>
 </template>
 <script>
+import SVGA from "svgaplayerweb";
+import testSVGA from "./test.svga";
+
 export default {
   data() {
-    return {
-      
-    }
+    return {};
   },
-  created() {
-    let parser = new svgaplay
-  },
+  mounted() {
+    let player = new SVGA.Player("#demo");
+    let parser = new SVGA.Parser("#demo");
+    parser.load(testSVGA, videoItem => {
+      player.setVideoItem(videoItem);
+      player.startAnimation();
+    });
+  }
 };
 </script>
+<style lang="scss">
+body {
+  background-color: #000;
+}
+#demo {
+  width: 100px;
+  height: 100%;
+}
+</style>
