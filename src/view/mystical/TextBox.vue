09d85fa00aab643b5348a3ccdf4ec2e5ed1aae1b
siykt <cnsiykt@163.com>
2019-09-19 12:51:50 +0800

    init Mystical page

---
+++ src/view/mystical/TextBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rule-text">
+    <div class="title">{{ ruleText.title }}</div>
+    <div class="text-box">
+      <p v-for="(text, index) in ruleText.texts" :key="index">{{ text }}</p>
+    </div>
+  </div>
+</template>
+<script>
+import ruleText from "./rule.json";
+export default {
+  data() {
+    return {
+      ruleText
+    };
+  }
+};
+</script>
---
+++ src/view/mystical/TitleBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="title">
+    <img class="logo" src="http://image.mjlive-app.com/page/mystical/logo.png" alt="logo" />
+    <img class="text" src="http://image.mjlive-app.com/page/mystical/text.png" alt="text" />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
