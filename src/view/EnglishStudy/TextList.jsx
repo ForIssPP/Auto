4693f846b04dab42556e9b76ca368fd78c33076b
siykt <cnsiykt@163.com>
2019-10-17 17:47:05 +0800

    Update TextList.jsx

--- src/view/EnglishStudy/TextList.jsx
+++ src/view/EnglishStudy/TextList.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export default function (props) {
 
-    const name = props.name || '';
+    const {name, class_name} = props;
 
     if (props.onClick) {
         return (
-            <li onClick={props.onClick}>
+            <li onClick={props.onClick} className={class_name}>
                 {name}
             </li>
         );
     } else {
         return (
-            <li>
+            <li className={class_name}>
                 {name}
             </li>
         );
