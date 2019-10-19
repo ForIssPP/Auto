8e8dbbbe9aae359161fda350b2685640cce3cf27
siykt <cnsiykt@163.com>
2019-10-19 18:17:47 +0800

    feat: Download 页面逻辑完成

--- src/view/ftpView/Download.vue
+++ src/view/ftpView/Download.vue
@@ -1,17 +1,52 @@
 <template>
-  <div class="container">
-    <Card :cardHeader="'文件名'">
-      <div>描述</div>
-      <button class="btn btn-primary">下载</button>
-    </Card>
+  <div class="container download">
+    <div class="row justify-content-center">
+      <div
+        class="col-lg-3 col-md-4 col-sm-6 col-8"
+        v-for="({fileName, fileTypeSrc, description, downloadId}, index) in fileInfoList"
+        :key="index"
+      >
+        <FileCard
+          :filerName="fileName"
+          :src="fileTypeSrc"
+          :description="description"
+          :download="downloadHandle(downloadId)"
+        ></FileCard>
+      </div>
+    </div>
+    <iframe v-show="false" :src="openIframeSrc"></iframe>
   </div>
 </template>
 <script>
-import Card from "./Card.vue";
+import FileCard from "./FileCard.vue";
+import getApi from "./getApi";
 
 export default {
+  data() {
+    return {
+      fileInfoList: [],
+      openIframeSrc: ""
+    };
+  },
+  methods: {
+    downloadHandle(fileId) {
+      return () => {
+        this.openIframeSrc = "/" + fileId;
+      };
+    },
+    getFileInfoList() {
+      getApi("fileList").done(res => {
+        if (res.code === 200) {
+          this.fileInfoList = res.data;
+        }
+      });
+    }
+  },
   components: {
-    Card
+    FileCard
+  },
+  created() {
+    this.getFileInfoList();
   }
 };
 </script>
