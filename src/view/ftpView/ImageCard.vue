2207223ba4b45a024b3e9d200fecdc822c125917
siykt <cnsiykt@163.com>
2019-10-19 18:16:04 +0800

    Create ImageCard component

---
+++ src/view/ftpView/ImageCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <loading :show="!show"></loading>
+    <img :src="src" :alt="alt" class="card-img-top" />
+    <div class="card-body">
+      <slot></slot>
+    </div>
+    <div v-if="cardFooter" class="card-footer" v-html="cardFooter"></div>
+  </div>
+</template>
+<script>
+import onLoadImages from "../../modules/onLoadImages";
+import loading from "../page/loading.vue";
+
+export default {
+  props: {
+    src: {},
+    alt: {
+      type: String,
+      default: "Image"
+    },
+    cardFooter: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      show: true
+    };
+  },
+  created() {
+    // onLoadImages(this.src).onload(() => (this.show = true));
+  },
+  components: {
+    loading
+  }
+};
+</script>
